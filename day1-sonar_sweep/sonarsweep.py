86e1f6b079f143924eaf8b413bfeb2dc1e7e3257
Andrew Cousins <andrew@elevateapp.com>
2021-12-01 09:29:47 -0800

    Part 2

--- day1-sonar_sweep/sonarsweep.py
+++ day1-sonar_sweep/sonarsweep.py
@@ -18,31 +18,29 @@ for newValue in data:
 print(increaseCount)
 
 
-# print("Part 2:")
-#
-# windows = []
-# windowIndex = 0
-#
-# with open('data.txt') as data:
-#     lines = data.readlines()
-#     maxWindowIndex = len(lines) - 2
-#     for newValue in lines:
-#         newValue = int(newValue)
-#         windows[windowIndex] += newValue
-#         windows[windowIndex+1] += newValue
-#         windows[windowIndex+2] += newValue
-#
-#         windowIndex += 1
-#         if windowIndex >= maxWindowIndex:
-#             break
-#
-# increaseCount = 0
-# lastValue = None
-#
-# for newValue in windows:
-#     if lastValue is not None:
-#         if newValue > lastValue:
-#             increaseCount += 1
-#     lastValue = newValue
-#
-# print(increaseCount)
+print("Part 2:")
+
+maxWindowIndex = len(data) - 2
+windows = [0 for value in data]
+windowIndex = 0
+
+for newValue in data:
+    newValue = int(newValue)
+    windows[windowIndex] += newValue
+    windows[windowIndex+1] += newValue
+    windows[windowIndex+2] += newValue
+
+    windowIndex += 1
+    if windowIndex >= maxWindowIndex:
+        break
+
+increaseCount = 0
+lastValue = None
+
+for newValue in windows:
+    if lastValue is not None:
+        if newValue > lastValue:
+            increaseCount += 1
+    lastValue = newValue
+
+print(increaseCount)
