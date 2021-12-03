ee3ab7e4427d4268d85565c630a24cdea56bbdaf
Andrew Cousins <andrew@elevateapp.com>
2021-12-03 10:00:18 -0800

    day 3 part 2

--- day3-binary_diagnostic/binarydiagnostic.py
+++ day3-binary_diagnostic/binarydiagnostic.py
@@ -32,3 +32,41 @@ print(gamma, epsilon, gamma*epsilon)
 
 
 print("Part 2")
+
+
+def narrow_down_values(values, index, useMoreFrequent):
+    validIndex = (useMoreFrequent == is_true_more_frequent(values, index))
+    newValues = []
+    for value in values:
+        if value[index] == validIndex:
+            newValues.append(value)
+    return newValues
+
+
+def binary_to_decimal(array):
+    value = 0
+    for i in range(len(array)):
+        decimalValue = pow(2, i)
+        arrayIndex = len(array) - 1 - i
+        if array[arrayIndex]:
+            value += decimalValue
+    return value
+
+
+oxygenValues = data.copy()
+index = 0
+while len(oxygenValues) > 1:
+    oxygenValues = narrow_down_values(oxygenValues, index, True)
+    index += 1
+
+oxygen = binary_to_decimal(oxygenValues[0])
+
+co2Values = data.copy()
+index = 0
+while len(co2Values) > 1:
+    co2Values = narrow_down_values(co2Values, index, False)
+    index += 1
+
+co2 = binary_to_decimal(co2Values[0])
+
+print(oxygen, co2, oxygen*co2)
