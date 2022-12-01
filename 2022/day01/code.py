a869620a9140d023623d740d2650580b13978af3
Andrew Cousins <andrew.cousins@elevatelabs.com>
2022-12-01 09:05:07 -0800

    Day 1

---
+++ 2022/day01/code.py
@@ -0,0 +1,30 @@
+elvenInventories = []
+elfInventory = []
+with open('input.txt') as data:
+    for value in data.readlines():
+        if value == '\n':
+            elvenInventories.append(elfInventory)
+            elfInventory = []
+        else:
+            elfInventory.append(int(value))
+if len(elfInventory) > 0:
+    elvenInventories.append(elfInventory)
+
+calorieCounts = []
+for elfInventory in elvenInventories:
+    calorieCount = 0
+    for calories in elfInventory:
+        calorieCount += calories
+    calorieCounts.append(calorieCount)
+calorieCounts.sort(reverse=True)
+
+print("Part 1:")
+print(calorieCounts[1])
+
+print("\nPart 2:")
+
+topCalorieSum = 0
+elvesToCount = 3
+for i in range(elvesToCount):
+    topCalorieSum += calorieCounts[i]
+print(topCalorieSum)
---
+++ 2022/day04/code.py
@@ -0,0 +1,6 @@
+data = []
+with open('input.txt') as data:
+    data = [int(value) for value in data.readlines()]
+
+
+print("Part 1:")
