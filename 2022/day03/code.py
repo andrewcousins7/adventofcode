36f5965e2128b51e1461aabd0c5f618dc6352e7b
Andrew Cousins <46725726+andrewcousins7@users.noreply.github.com>
2022-12-03 22:26:55 -0800

    2022 Day 3

--- 2022/day03/code.py
+++ 2022/day03/code.py
@@ -1,6 +1,42 @@
-data = []
+rucksacks = []
 with open('input.txt') as data:
-    data = [int(value) for value in data.readlines()]
+    rucksacks = [value.strip() for value in data.readlines()]
 
 
 print("Part 1:")
+
+totalPriority = 0
+for items in rucksacks:
+    halfItemCount = int(len(items) / 2)
+    compartmentOne = items[:halfItemCount]
+    compartmentTwo = items[halfItemCount:]
+
+    for item in compartmentOne:
+        if item in compartmentTwo:
+            priority = 0
+            if item.isupper():
+                priority = ord(item) - 38
+            else:
+                priority = ord(item) - 96
+            totalPriority += priority
+            break
+
+print(totalPriority)
+
+print("Part 2:")
+totalPriority = 0
+pointer = 0
+while pointer < len(rucksacks):
+    for item in rucksacks[pointer]:
+        if item in rucksacks[pointer+1]:
+            if item in rucksacks[pointer+2]:
+                priority = 0
+                if item.isupper():
+                    priority = ord(item) - 38
+                else:
+                    priority = ord(item) - 96
+                totalPriority += priority
+                break
+    pointer += 3
+
+print(totalPriority)
