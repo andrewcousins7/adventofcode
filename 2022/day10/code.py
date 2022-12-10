3dedcc4e7afe1c50eb13d47eb71ca55d0307172f
Andrew Cousins <46725726+andrewcousins7@users.noreply.github.com>
2022-12-10 15:23:43 -0800

    2022 Day 10

--- 2022/day10/code.py
+++ 2022/day10/code.py
@@ -1,6 +1,41 @@
-data = []
+signals = []
 with open('input.txt') as data:
-    data = [int(value) for value in data.readlines()]
-
+    for line in data.readlines():
+        signals.append(line.strip().split(" "))
 
 print("Part 1:")
+cycles = []
+register = 1
+for signal in signals:
+    cycles.append(register)
+    if signal[0] == "addx":
+        cycles.append(register)
+        register += int(signal[1])
+
+print(cycles)
+
+signalStrengthSum = 0
+i = 19
+while i < len(cycles):
+    signalStrengthSum += (i+1) * cycles[i]
+    print(i, cycles[i])
+    i += 40
+
+print(signalStrengthSum)
+
+print("Part 2:")
+
+for row in range(6):
+    output = ""
+    for col in range(40):
+        cycleIndex = (row * 40) + col
+        cycleValue = cycles[cycleIndex]
+        spriteStart = cycleValue - 1
+        spriteEnd = cycleValue + 1
+        if spriteStart <= col <= spriteEnd:
+            output += "#"
+        else:
+            output += "."
+        #print(cycleIndex+1, col, cycles[cycleIndex])
+        #print(output)
+    print(output)
